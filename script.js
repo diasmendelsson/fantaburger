53d43b3ab9eac4acfbd9bfec5a5a446a841618b8
diasmendelsson <mendersson@hotmail.com>
2026-01-25 20:43:14 -0300

    Cardápio

---
+++ script.js
@@ -0,0 +1,180 @@
+const telefoneWhatsApp = "5517996567779"; // coloque seu número
+
+
+const combos = [
+  {
+    nome: "Combo 2 X-Salada",
+    descricao: "Pão, maionese, hambúrguer 120g, presunto, mussarela, milho, ervilha, alface, tomate, batata palha + Guaraná Poty 2L",
+    preco: 40.00,
+    imagem: "/public/combo.jpg"
+  },
+  {
+    nome: "Combo 2 X-Salada Bacon",
+    descricao: "Pão, maionese, hambúrguer 120g, presunto, mussarela, bacon, milho, ervilha, alface, tomate, batata palha + Guaraná Poty 2L",
+    preco: 40.99,
+    imagem: "/public/combo.jpg"
+  },
+  {
+    nome: "Combo 2 X-Salada Egg",
+    descricao: "Pão, maionese, hambúrguer, presunto, mussarela, ovo, milho, ervilha, alface, tomate, batata palha + Guaraná Poty 2L",
+    preco: 42.99,
+    imagem: "/public/combo.jpg"
+  },
+  {
+    nome: "Combo 2 X-Egg",
+    descricao: "Pão, maionese, hambúrguer 120g, ovo, presunto, mussarela, batata palha + Guaraná Poty 2L",
+    preco: 41.99,
+    imagem: "/public/combo.jpg"
+  },
+  {
+    nome: "Combo 2 X-Egg Bacon",
+    descricao: "Pão, maionese, hambúrguer 120g, ovo, bacon, presunto, mussarela, batata palha + Guaraná Poty 2L",
+    preco: 45.99,
+    imagem: "/public/combo.jpg"
+  },
+  {
+    nome: "Combo 2 X-Bacon",
+    descricao: "Pão, maionese, hambúrguer 120g, bacon, presunto, mussarela, batata palha + Guaraná Poty 2L",
+    preco: 43.99,
+    imagem: "/public/combo.jpg"
+  },
+  {
+    nome: "Combo 2 X-Tudo",
+    descricao: "Pão, maionese, hambúrguer 120g, ovo, bacon, presunto, mussarela, alface, tomate, milho, ervilha, batata palha + Guaraná Poty 2L",
+    preco: 48.99,
+    imagem: "/public/combo.jpg"
+  },
+  {
+    nome: "Combo 2 X-Tudo Duplo",
+    descricao: "Pão, maionese, 2 hambúrgueres, 2 ovos, 2 presuntos, 2 mussarelas, bacon, milho, ervilha, alface, tomate, batata palha + Guaraná Poty 2L",
+    preco: 58.99,
+    imagem: "/public/combo.jpg"
+  }
+];
+
+const cardapio = document.getElementById("cardapio");
+const listaCarrinho = document.getElementById("listaCarrinho");
+const totalEl = document.getElementById("total");
+const qtdCarrinho = document.getElementById("qtdCarrinho");
+
+let carrinho = [];
+
+// Renderizar carrinho
+
+combos.forEach((combo, index) =>{
+    cardapio.innerHTML += `
+      <article class="card">
+          <img class="card-img" src="${combo.imagem}" alt="${combo.nome}"/>
+          <div class="card-content">
+            <h2>${combo.nome}</h2>
+            <p class="article_p">${combo.descricao}</p>
+            <p class="article_p">1 Guaraná Poty 2L</p>
+            <button onclick="adicionarCarrinho(${index})" class="div1" ><span class="radio"><img src="/public/plus.svg" /></span> <p class="span_p"><span></span>${formatarReal(combo.preco)}<span></span></p></button>
+          </div>
+          
+        </article>
+    `
+});
+
+function formatarReal(valor) {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(valor);
+}
+
+function adicionarCarrinho(index){
+     const item = carrinho.find(i => i.nome === combos[index].nome);
+
+     if(item) {
+        item.qtd++;
+     } else{
+        carrinho.push({
+            nome: combos[index].nome,
+            preco: combos[index].preco,
+            qtd: 1
+        })
+     }
+
+     atualizarCarrinho();
+}
+
+//Atualizar carrinho
+function atualizarCarrinho(){
+    listaCarrinho.innerHTML = "";
+    let total = 0;
+    let quantidadeTotal = 0;
+
+    carrinho.forEach((item, index) =>{
+
+        total += item.preco * item.qtd;
+        quantidadeTotal += item.qtd;
+
+        listaCarrinho.innerHTML +=`
+            <li class="lista-car">
+              <span>${item.nome}</span>
+              <div>
+                <button class="btn-menos" onclick="diminuir(${index})"><img class="icon" src="/public/minus.svg" alt="icone de menos"/></button>
+                <span>${item.qtd}</span>
+                <button class="btn-mais" onclick="aumentar(${index})"><img class="icon" src="/public/plus.svg" alt="icone de mais"/></button>
+                <button class="btn-remover" onclick="remover(${index})"><img class="icon" src="/public/trash.svg" alt="icone de lixeira"/></button>
+              </div>
+            </li> 
+        `
+    })
+
+      totalEl.textContent = formatarReal(total);
+      qtdCarrinho.textContent = quantidadeTotal;
+}
+
+
+// Controles 
+function aumentar(index) {
+    carrinho[index].qtd++;
+    atualizarCarrinho();
+}
+
+function diminuir(index){
+    if(carrinho[index].qtd > 1) {
+      carrinho[index].qtd--;
+    } else{
+        carrinho.splice(index, 1)
+    }
+    atualizarCarrinho();
+}
+
+function remover(index){
+    carrinho.splice(index, 1);
+    atualizarCarrinho();
+}
+
+
+// Abrir e fechar carrinho
+
+function toggleCarrinho(){
+    document.getElementById("carrinho").classList.toggle("ativo");
+    document.getElementById("overlay").style.display = 
+     document.getElementById("carrinho").classList.contains("ativo")
+       ? "block"
+       : "none";
+}
+
+
+// WhatsApp
+function enviarWhatsApp() {
+  if (carrinho.length === 0) {
+    alert("Carrinho vazio!");
+    return;
+  }
+
+  let mensagem = "🛒 *Pedido*\n\n";
+
+  carrinho.forEach(item => {
+    mensagem += `• ${item.nome} (${item.qtd}x) - R$ ${(item.preco * item.qtd).toFixed(2)}\n`;
+  });
+
+  mensagem += `\n💰 *Total:* ${totalEl.textContent}`;
+
+  const url = `https://wa.me/${telefoneWhatsApp}?text=${encodeURIComponent(mensagem)}`;
+  window.open(url, "_blank");
+}
